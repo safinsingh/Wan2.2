8f3117ae02211f35592b18ea6e5a5e765a2fdba5
Safin Singh <safin.singh@gmail.com>
2026-03-13 15:14:10 -0700

    [mlab] lazy imports

--- wan/__init__.py
+++ wan/__init__.py
@@ -1,13 +1,18 @@
 # Copyright 2024-2025 The Alibaba Wan Team Authors. All rights reserved.
 from . import configs, distributed, modules
-from .image2video import WanI2V
-from .text2video import WanT2V
-from .textimage2video import WanTI2V
-from .animate import WanAnimate
+
+_lazy_imports = {
+    "WanI2V": ".image2video",
+    "WanS2V": ".speech2video",
+    "WanT2V": ".text2video",
+    "WanTI2V": ".textimage2video",
+    "WanAnimate": ".animate",
+}
 
 
 def __getattr__(name):
-    if name == "WanS2V":
-        from .speech2video import WanS2V
-        return WanS2V
+    if name in _lazy_imports:
+        import importlib
+        module = importlib.import_module(_lazy_imports[name], __name__)
+        return getattr(module, name)
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
