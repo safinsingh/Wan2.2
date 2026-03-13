7f3e7ebcdc21313ad0d811c36c0748567794089e
Safin Singh <safin.singh@gmail.com>
2026-03-13 15:13:16 -0700

    [mlab] conditionally import s2v deps

--- wan/__init__.py
+++ wan/__init__.py
@@ -1,7 +1,13 @@
 # Copyright 2024-2025 The Alibaba Wan Team Authors. All rights reserved.
 from . import configs, distributed, modules
 from .image2video import WanI2V
-from .speech2video import WanS2V
 from .text2video import WanT2V
 from .textimage2video import WanTI2V
 from .animate import WanAnimate
+
+
+def __getattr__(name):
+    if name == "WanS2V":
+        from .speech2video import WanS2V
+        return WanS2V
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
